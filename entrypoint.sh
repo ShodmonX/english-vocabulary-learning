91528716dfd6122769a7eb1d7df93c25b7dc08fa
ShodmonX <xolmurodovshodmon@gmail.com>
2026-01-16 00:58:50 +0500

    fix: dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 alembic upgrade head
-# exec python -m app.main
-exec watchfiles --filter python --target-type command "python -m app.main" /app
+exec python -m app.main
+# exec watchfiles --filter python --target-type command "python -m app.main" /app
 
